e0ca624e0bf0594f00e1e2a9a734c99bede9ff6d
mjmac <mjmac@E0C9>
2024-09-10 09:54:53 +0900

    new

--- 5701576_09_10.c
+++ 5701576_09_10.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct {
+typedef struct TreNode{
 	int data;
-	TreeNode* left, * right;
+	struct TreeNode* left, * right;
 
 }TreeNode;
 
@@ -13,5 +13,13 @@ main() {
 	TreeNode n1, n2, n3, n4, n5, n6, n7, n8;
 
 	n1.data = 10, n1.left = &n2, n1.right = &n3;
+	n2.data = 5, n2.left = &n4, n2.right = &n5;
+	n3.data = 9, n3.left = NULL, n3.right = &n6;
+	n4.data = 6, n4.left = NULL, n4.right = NULL;
+	n5.data = 3, n5.left = NULL, n5.right = NULL;
+	n6.data = 7, n6.left = NULL, n6.right = NULL;
+}
+
+void Pre_Order_By_Printer(TreeNode *np) {
 
 }
